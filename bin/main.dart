71477fa1c8c8f9c16bd7a7e7fb1891c50694a323
tamim hasan <tamimh40@gmail.com>
2024-06-24 23:19:56 +0600

    update comment and screen short

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,16 @@
 import 'package:live_test/media.dart';
 import 'package:live_test/song.dart';
 
+/* This is the first Live Test of ostad flutter batch seven
+  * Completed by TAMIM HASAN 
+  * Date: 24-06-2024
+  */
 void main() {
-  // Create an instance of Media
+  //-- Create an instance of Media
   Media media = Media();
   media.play(); // Should print "Playing media..."
 
-  // Create an instance of Song
-  Song song = Song('Artist Name');
+  //-- Create an instance of Song
+  Song song = Song('Junaed Jamshed');
   song.play(); // Should print "Playing song by Artist Name..."
 }
